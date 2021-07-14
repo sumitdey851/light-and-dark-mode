2cb33203cfeb46b9bdbb5b4e221e2fc6430dacba
sumitdey851 <sumitdey851@outlook.com>
2021-07-14 13:18:38 +0530

    added functionality of theme switching

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Get elements from DOM
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const coder = document.getElementById('coder');
+const designer = document.getElementById('designer');
+const reader = document.getElementById('reader');
+const cook = document.getElementById('cook');
+const gamer = document.getElementById('gamer');
+const music = document.getElementById('music');
+const travel = document.getElementById('travel');
+const textBox = document.getElementById('description');
+
+// Dark or Light Images
+function imageMode(color) {
+    coder.src = `img/coder_${color}.svg`;
+    designer.src = `img/ui_design_${color}.svg`;
+    reader.src = `img/reading_${color}.svg`;
+    cook.src = `img/cooking_${color}.svg`;
+    gamer.src = `img/gaming_${color}.svg`;
+    music.src = `img/music_${color}.svg`;
+    travel.src = `img/travel_${color}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+// Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
