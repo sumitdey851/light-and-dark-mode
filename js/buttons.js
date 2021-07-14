18a32bad35065b9a9182f17f6fa0732776be238e
sumitdey851 <sumitdey851@outlook.com>
2021-07-14 14:50:05 +0530

    Added two new projects to projects section

--- js/buttons.js
+++ js/buttons.js
@@ -4,6 +4,8 @@ const btnQuoteGenerator = document.getElementById('quote-generator');
 const btnInfiniteScroll = document.getElementById('infinite-scroll');
 const btnJokeTeller = document.getElementById('joke-teller');
 const btnPip = document.getElementById('picture-in-picture');
+const btnWeather = document.getElementById('weather');
+const btnChat = document.getElementById('chat');
 
 // Button functions
 function gotoRepos() {
@@ -31,9 +33,21 @@ function gotoPipApp() {
     window.open(url, '_blank');
 }
 
+function gotoWeatherApp() {
+    const url = 'https://killgod-weather-app.herokuapp.com/';
+    window.open(url, '_blank');
+}
+
+function gotoChatApp() {
+    const url = 'https://killgod-chat-app.herokuapp.com/';
+    window.open(url, '_blank');
+}
+
 // Button behaviour
 btnGetSource.addEventListener('click', gotoRepos);
 btnQuoteGenerator.addEventListener('click', gotoQuoteGeneratorApp);
 btnInfiniteScroll.addEventListener('click', gotoInfiniteScrollApp);
 btnJokeTeller.addEventListener('click', gotoJokeTellerApp);
 btnPip.addEventListener('click', gotoPipApp);
+btnWeather.addEventListener('click', gotoWeatherApp);
+btnChat.addEventListener('click', gotoChatApp);
